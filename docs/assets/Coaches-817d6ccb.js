8c2f4218ba6cef34091656312c5259df867afc48
Nikolas Lyons <nikolas.lyons23@gmail.com>
2023-11-19 03:16:09 -0700

    pleasse build

---
+++ docs/assets/Coaches-817d6ccb.js
@@ -0,0 +1,1 @@
+import{_ as o,r as s,o as t,c,w as a,d as n}from"./index-61bc65b3.js";const r={},_=n("h1",{class:"text-center"},"Coaches Bio's Coming soon!",-1);function d(h,i){const e=s("v-card");return t(),c(e,{class:"mx-auto",width:"600",height:"600"},{default:a(()=>[_]),_:1})}const l=o(r,[["render",d]]);export{l as default};
